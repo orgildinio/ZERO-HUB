0a16deb2997cc249966085ab7a66d2d1b3fe5f44
Ashish1022 <ashishjadhav9900@gmail.com>
2025-06-06 14:52:14 +0530

    tenant caching in redis

--- src/modules/tenants/server/procedure.ts
+++ src/modules/tenants/server/procedure.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import redis from "@/lib/redis";
 
 export const tenantsRouter = createTRPCRouter({
     getOne: baseProcedure
@@ -10,21 +11,47 @@ export const tenantsRouter = createTRPCRouter({
             })
         )
         .query(async ({ ctx, input }) => {
+            const cacheKey = `tenant:${input.slug}`;
 
-            const tenant = await ctx.db.find({
-                collection: "tenants",
-                depth: 0,
-                limit: 1,
-                pagination: false,
-                where: {
-                    slug: {
-                        equals: input.slug
-                    }
+            try {
+                const cached = await redis.get(cacheKey);
+                if (cached) {
+                    return cached;
                 }
-            });
-            const data = tenant.docs[0].store
-            if (!data) return null;
+                const tenant = await ctx.db.find({
+                    collection: "tenants",
+                    depth: 0,
+                    limit: 1,
+                    pagination: false,
+                    where: {
+                        slug: {
+                            equals: input.slug
+                        }
+                    }
+                });
+                const data = tenant.docs[0].store
+                if (!data) {
+                    await redis.setex(cacheKey, 300, JSON.stringify(null));
+                    return null;
+                }
 
-            return data;
+                await redis.setex(cacheKey, 3600, data);
+
+                return data;
+            } catch {
+                const tenant = await ctx.db.find({
+                    collection: "tenants",
+                    depth: 0,
+                    limit: 1,
+                    pagination: false,
+                    where: {
+                        slug: {
+                            equals: input.slug
+                        }
+                    }
+                });
+                const data = tenant.docs[0].store
+                return data
+            }
         })
 })
