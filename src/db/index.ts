2e42d799bd53f06ce2ee80902b166277e88b768e
Ashish1022 <ashishjadhav9900@gmail.com>
2025-06-28 19:55:31 +0530

    Database pooling optimization

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   
   max: 20,  
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 20000,
   
   keepAlive: true,
   keepAliveInitialDelayMillis: 10000,
